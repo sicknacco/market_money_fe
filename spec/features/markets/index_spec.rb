d1a77e376b29331676d80326ef3ef15e96d2979e
Nick Sacco <nicksacco10@gmail.com>
2023-09-07 12:46:24 -0700

    test: markets index tests started

---
+++ spec/features/markets/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Markets index page' do
+  describe 'As a visitor' do
+    it 'I see the name, city, and state of each market in the system' do
+      visit '/markets'
+
+      expect(page).to have_content('Markets')
+      within '#markets' do
+        expect(page).to have_content('Name')
+        expect(page).to have_content('City')
+        expect(page).to have_content('State')
+      end
+    end
+  end
+end
