cc1e900110932ab78af9f15b2f5622f687eb7e56
Nick Sacco <nicksacco10@gmail.com>
2023-09-11 13:08:03 -0700

    search wip

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,6 +1,14 @@
 class VendorsController < ApplicationController
+  # before_action :permit_params, only: [:show]
+
   def show
     @vendor = VendorFacade.get_vendor(params[:id])
     @markets = MarketFacade.returned_markets(params[:name], params[:city], params[:state])
   end
+  
+  private
+  
+  # def permit_params
+  #   params.permit(:name, :city, :state)
+  # end
 end
